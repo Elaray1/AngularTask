2d6b4b2731d865393f1a0ca0ab805b01fd77b2da
Egor <5642452@mail.ru>
2019-07-11 17:24:56 +0300

    taskAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { FormsModule } from '@angular/forms';
+import { TodosFilterPipe } from './shared/todos.filter.pipe';
+import { PhonePipeComponent } from './shared/phone.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    TodosFilterPipe,
+    PhonePipeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
